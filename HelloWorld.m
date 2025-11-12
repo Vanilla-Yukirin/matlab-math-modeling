e6314c2280666247b1690d0bc83202c6dd5525e9
Vanilla <heshaohong2015@outlook.com>
2025-11-12 17:49:03 +0800

    upd: 完成40%

--- HelloWorld.m
+++
@@ -1,3 +0,0 @@
-n=input("请输入一个数字：");
-n
-fprintf("Hello, World!\n")
---
+++ workspace/HelloWorld.m
@@ -0,0 +1,5 @@
+clc; % clear console
+clear; % clear variables
+n=input("Please input a number: ");
+n
+fprintf("Hello, World!\n")
